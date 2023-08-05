c2544470eef96f99b47418f50f6def386885f681
kennycormic <95879943+kennycormic@users.noreply.github.com>
2023-08-05 16:41:18 +0330

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xbb8225619a2b8deb6f978c9ef6e5ab6ead43ac85";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
